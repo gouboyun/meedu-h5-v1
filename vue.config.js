5568afa1f4395f6dda4ad8c8c3f8de084e4d4171
none <none>
2022-03-28 14:37:11 +0800

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,14 @@
 module.exports = {
-    publicPath: '',
-    productionSourceMap: false,
-    assetsDir: 'static',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://192.168.3.8:8000',
-                changeOrigin: true,
-                ws: true
-            },
-            '/addons': {
-                target: 'http://192.168.3.8:8000',
-                changeOrigin: true,
-                ws: true
-            }
-        }
-    },
-}
+  publicPath: "",
+  productionSourceMap: false,
+  assetsDir: "static",
+  //   devServer: {
+  //     proxy: {
+  //       "/api": {
+  //         target: "http://192.168.3.8:8000",
+  //         changeOrigin: true,
+  //         ws: true,
+  //       },
+  //     },
+  //   },
+};
