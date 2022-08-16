7d6a5b3a4eda115efb21741fc811416a7ad75c47
禺狨 <18119604035@163.com>
2022-08-16 15:32:06 +0800

    个人资料绑定社交登录逻辑优化

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default {
       this.setConfig(res.data);
       if (!this.$utils.isMobile()) {
         if (res.data.pc_url !== "") {
-          // window.location.href = res.data.pc_url;
+          window.location.href = res.data.pc_url;
         }
       }
     },
